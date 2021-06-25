8c82885081b028d6cbcd04106d96ae608771f4af
jimmyjansenvantland <info@mejimmy.com>
2021-06-25 12:11:53 +0200

    anotherupdate

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -4,9 +4,9 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-	// console.log(myOtherBox.object3D.rotation);
+	// myOtherBox.object3D.rotation.y += rotationSpeed;
+	// myOtherBox.object3D.rotation.z += rotationSpeed/2;
+	console.log(myOtherBox.object3D.rotation);
 }
 
 setInterval(spin, 16);
